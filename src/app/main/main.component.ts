54d7c4b716b30825614d4a3fa2df232272161e36
Katy Daviscourt <katy.daviscourt@gmail.com>
2017-04-07 12:00:14 -0700

    add edit form but it does not yet connect to the database

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -13,6 +13,7 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 export class MainComponent implements OnInit {
   rowers: FirebaseListObservable<any[]>;
+  currentRoute: string = this.router.url;
 
   constructor(private router: Router, private rowerService: RowerService){}
 
